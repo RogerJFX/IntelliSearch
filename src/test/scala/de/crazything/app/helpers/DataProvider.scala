4ddf3d4006f50b58d03466caf23e13e8f88191a2
Roger <roger@crazything.de>
2018-07-23 16:45:41 +0200

    Minor refactoring

--- src/test/scala/de/crazything/app/helpers/DataProvider.scala
+++ src/test/scala/de/crazything/app/helpers/DataProvider.scala
@@ -11,10 +11,10 @@ object DataProvider {
 
   def readPersons(): Seq[Person] = {
     import scala.collection.JavaConverters._
-    val url: URL = DataProvider.getClass.getResource("/data.txt")
+    val url: URL = DataProvider.getClass.getResource("/persons.txt")
     val path: Path = Paths.get(url.toURI)
     val lines: Seq[String] = Files.readAllLines(path).asScala
-    val buffer: ListBuffer[Person] =  new ListBuffer
+    val buffer: ListBuffer[Person] = new ListBuffer
     lines.foreach(line => {
       val arr: Array[String] = line.split(";")
       buffer.append(Person(arr(0).toInt, arr(1), arr(2), arr(3), arr(4), arr(5)))

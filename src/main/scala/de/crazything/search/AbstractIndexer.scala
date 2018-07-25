6c8a45c51f78d190ec16cc997467d39678755d25
Roger <roger@crazything.de>
2018-07-25 09:29:34 +0200

    Cleaned AbstractIndexer

--- src/main/scala/de/crazything/search/AbstractIndexer.scala
+++ src/main/scala/de/crazything/search/AbstractIndexer.scala
@@ -1,8 +1,5 @@
 package de.crazything.search
 
-import java.net.URL
-import java.nio.file.{Path, Paths}
-
 import org.apache.lucene.analysis.Analyzer
 import org.apache.lucene.document.Document
 import org.apache.lucene.index.{IndexWriter, IndexWriterConfig}
@@ -10,21 +7,15 @@ import org.apache.lucene.store._
 
 abstract class AbstractIndexer {
 
-//  private[this] val url: URL = classOf[AbstractIndexer].getResource("/")
-//  private[this] val target = new URL(url, "pindex")
-//  private[this] val path: Path = Paths.get(target.toURI)
-
   protected def createIndex[I, T <: PkDataSet[I]](analyzer: Analyzer, data: Seq[T], factory: AbstractTypeFactory[I, T]): Unit = {
     val config = new IndexWriterConfig(analyzer)
-    val directory = /*new SimpleFSDirectory(path)*/new RAMDirectory
+    val directory = new RAMDirectory
     val writer = new IndexWriter(directory, config)
     doIndex(writer, data, factory)
     writer.close()
     factory.setDataPool(data)
     putDirectoryReference(directory)
   }
-
-
 
   private def doIndex[I, T <: PkDataSet[I]](writer: IndexWriter, data: Seq[T], factory: AbstractTypeFactory[I, T]): Unit = {
     data.foreach(dataSet => {

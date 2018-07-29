138fa0457b9a2b3344f7906a3e2e43b54a7aa44c
Roger <roger@crazything.de>
2018-07-29 16:54:03 +0200

    Bad commit.

--- src/main/scala/de/crazything/search/CommonSearcherFiltered.scala
+++ src/main/scala/de/crazything/search/CommonSearcherFiltered.scala
@@ -5,18 +5,11 @@ import java.util.concurrent.atomic.AtomicInteger
 import de.crazything.search.entity.{PkDataSet, QueryCriteria, SearchResult}
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.concurrent.{Await, Future, Promise}
 import scala.util.{Failure, Success}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
-/*
-  * Global ExecutionContext did it in the end. I'm pissed at the moment. Was in trouble, since method
-  * searchAsyncAsync did not want to work.
-  *
-  * TODO: Just get calm again and sweep a little bit.
-  */
 /**
   * Combine searches with other filters, maybe other searches.
   */
@@ -37,9 +30,7 @@ object CommonSearcherFiltered {
                                         factory: AbstractTypeFactory[I, T],
                                         queryCriteria: Option[QueryCriteria] = None,
                                         maxHits: Int = MAGIC_NUM_DEFAULT_HITS,
-                                        filterFn: (SearchResult[I, T]) => Boolean)
-                                        //(implicit ec: ExecutionContext)
-  : Future[Seq[SearchResult[I, T]]] = {
+                                        filterFn: (SearchResult[I, T]) => Boolean): Future[Seq[SearchResult[I, T]]] = {
     val searchResult: Future[Seq[SearchResult[I, T]]] = CommonSearcher.searchAsync(input, factory, queryCriteria, maxHits)
     val promise: Promise[Seq[SearchResult[I, T]]] = Promise[Seq[SearchResult[I, T]]]
     searchResult.onComplete {
@@ -50,49 +41,75 @@ object CommonSearcherFiltered {
     promise.future
   }
 
+  /**
+    *
+    * Both, the search and filtering is async. Might once be helpful, if comparable data for filtering process
+    * comes from somewhere remote.
+    *
+    * Normally searchAsync will do, so if you plan to filter right here.
+    *
+    * @param input Search input
+    * @param factory Factory to be provided.
+    * @param queryCriteria Optional criteria to help factory to decide which query to select.
+    * @param maxHits Max hits of search process before filtering. Default 500.
+    * @param filterFn The filter function as future.
+    * @param filterTimeout Timeout for filtering. Keep in mind there may lots of async filter processes. This Duration
+    *                      covers the time of all(!) processes. So it should not be too small. Default is infinite.
+    * @tparam I PK type of data object
+    * @tparam T type of data object
+    * @return Sequence of SearchResult as Future.
+    */
   def searchAsyncAsync[I, T <: PkDataSet[I]](input: T,
                                              factory: AbstractTypeFactory[I, T],
                                              queryCriteria: Option[QueryCriteria] = None,
                                              maxHits: Int = MAGIC_NUM_DEFAULT_HITS,
                                              filterFn: (SearchResult[I, T]) => Future[Boolean],
-                                             filterTimeout: Duration = Duration.Inf)
-                                             //(implicit ec: ExecutionContext)
-  : Future[Seq[SearchResult[I, T]]] = {
+                                             filterTimeout: Duration = Duration.Inf): Future[Seq[SearchResult[I, T]]] = {
     val searchResult: Future[Seq[SearchResult[I, T]]] = CommonSearcher.searchAsync(input, factory, queryCriteria, maxHits)
     val promise: Promise[Seq[SearchResult[I, T]]] = Promise[Seq[SearchResult[I, T]]]
     searchResult.onComplete {
-      case Success(res) => promise.success(res.filter(sr => Await.result(filterFn(sr), filterTimeout)))
+//      case Success(res) =>
+//        promise.success(res.filter(sr => {
+//          // No need for sorting again here. It is not concurrent...
+//          Await.result(filterFn(sr), filterTimeout)
+//        }))
 //      case Success(res) => promise.success(for {
 //        x <- res if Await.result(filterFn(x), filterTimeout)
 //      } yield x)
-//      case Success(res) => promise.success(Await.result(doFilterAsync(res, filterFn), filterTimeout))
+      case Success(res) =>
+        val future: Future[Seq[SearchResult[I, T]]] = doFilterAsync(res, filterFn)
+        future.onComplete{
+          case Success(resF) => promise.success(resF)
+          case _ => throw new RuntimeException("Fuck it")
+        }
+       // promise.success(Await.result(doFilterAsync(res, filterFn), filterTimeout))
       case Failure(t) => throw new RuntimeException("Future failed in searchAsyncWithAsyncFilter", t)
       case _ => throw new RuntimeException("Unknown Error in searchAsyncWithAsyncFilter")
     }
     promise.future
   }
-
   // private desperation...
-//  private def doFilterAsync[I, T <: PkDataSet[I]](raw: Seq[SearchResult[I, T]],
-//                                                  filterFn: (SearchResult[I, T]) => Future[Boolean])
-//                                               //  (implicit ec: ExecutionContext)
-//  : Future[Seq[SearchResult[I, T]]] = {
-//    val promise: Promise[Seq[SearchResult[I, T]]] = Promise[Seq[SearchResult[I, T]]]
-//    val buffer: ListBuffer[SearchResult[I, T]] = ListBuffer[SearchResult[I, T]]()
-//    val counter = new AtomicInteger()
-//    val len = raw.length
-//    def checkLen(count: Int): Unit = if (count == len) promise.success(buffer)
-//    raw.foreach(sr => {
-//      filterFn(sr).onComplete {
-//        case Success(b) =>
-//          if (b) {
-//            buffer.append(sr)
-//          }
-//          checkLen(counter.incrementAndGet())
-//        case _ => throw new RuntimeException("Whatever. Later. I'm pissed at this very moment.")
-//      }
-//    })
-//    promise.future
-//  }
-
+    private def doFilterAsync[I, T <: PkDataSet[I]](raw: Seq[SearchResult[I, T]],
+                                                    filterFn: (SearchResult[I, T]) => Future[Boolean])
+    : Future[Seq[SearchResult[I, T]]] = {
+      val promise: Promise[Seq[SearchResult[I, T]]] = Promise[Seq[SearchResult[I, T]]]
+      val buffer: ListBuffer[SearchResult[I, T]] = ListBuffer[SearchResult[I, T]]()
+      val counter = new AtomicInteger()
+      val len = raw.length
+      def checkLen(count: Int): Unit = if (count == len) promise.success(buffer)
+      raw.foreach(sr => {
+        println(s"Before future $sr")
+        filterFn(sr).onComplete {
+          case Success(troo) =>
+            if (troo) {
+              println(s"FUTURE SUCCESS $sr")
+              buffer.append(sr)
+            }
+            checkLen(counter.incrementAndGet())
+          case _ => throw new RuntimeException("Whatever. Later. I'm pissed at this very moment.")
+        }
+        println("After future")
+      })
+      promise.future
+    }
 }
--- src/test/scala/de/crazything/app/test/FilterAsyncTest.scala
+++ src/test/scala/de/crazything/app/test/FilterAsyncTest.scala
@@ -2,11 +2,13 @@ package de.crazything.app.test
 
 import de.crazything.app.{GermanLanguage, Person, PersonFactoryDE}
 import de.crazything.app.test.helpers.DataProvider
-import de.crazything.search.entity.SearchResult
-import de.crazything.search.{CommonIndexer, CommonSearcherFiltered, QueryConfig}
+import de.crazything.search.entity.{PkDataSet, QueryCriteria, SearchResult}
+import de.crazything.search.{AbstractTypeFactory, CommonIndexer, CommonSearcherFiltered, QueryConfig}
+import org.apache.lucene.document.Document
+import org.apache.lucene.search.Query
 import org.scalatest.{AsyncFlatSpec, BeforeAndAfter}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class FilterAsyncTest extends AsyncFlatSpec with BeforeAndAfter with QueryConfig with GermanLanguage {
 
@@ -19,36 +21,100 @@ class FilterAsyncTest extends AsyncFlatSpec with BeforeAndAfter with QueryConfig
     filterFrankfurt(result)
   }
 
+
   before {
     CommonIndexer.index(DataProvider.readVerySimplePersons(), PersonFactoryDE)
   }
 
-  "Async search with sync filter" should "exclude Mayer living not in Frankfurt" in {
-    CommonSearcherFiltered.searchAsync(input = standardPerson.copy(lastName = "Mayer"), factory = PersonFactoryDE,
-      filterFn = filterFrankfurt).map(result => {
-      assert(result.isEmpty)
-    })
-  }
+  //  "Async search with sync filter" should "exclude Mayer living not in Frankfurt" in {
+  //    CommonSearcherFiltered.searchAsync(input = standardPerson.copy(lastName = "Mayer"), factory = PersonFactoryDE,
+  //      filterFn = filterFrankfurt).map(result => {
+  //      assert(result.isEmpty)
+  //    })
+  //  }
+  //
+  //  it should "pass Hösl living in Frankfurt" in {
+  //    CommonSearcherFiltered.searchAsync(input = standardPerson.copy(lastName = "Hösl"), factory = PersonFactoryDE,
+  //      filterFn = filterFrankfurt).map(result => {
+  //      assert(result.length == 1)
+  //    })
+  //  }
+  //
+  //  "Async search with async filter" should "exclude Mayer living not in Frankfurt" in {
+  //    import scala.concurrent.duration._
+  //    CommonSearcherFiltered.searchAsyncAsync(input = standardPerson.copy(lastName = "Mayer"), factory = PersonFactoryDE,
+  //      filterFn = filterFrankfurtAsync, filterTimeout = 10.seconds).map(result => {
+  //      assert(result.isEmpty)
+  //    })
+  //  }
+  //
+  //  it should "pass Hösl living in Frankfurt" in {
+  //    CommonSearcherFiltered.searchAsyncAsync(input = standardPerson.copy(lastName = "Hösl"), factory = PersonFactoryDE,
+  //      filterFn = filterFrankfurtAsync).map(result => {
+  //      assert(result.length == 1)
+  //    })
+  //  }
+  // Make sure async processing does not destroy order of results.
+  "Results" should "be sorted properly" in {
+    object PersonFactoryAll extends AbstractTypeFactory[Int, Person] {
 
-  it should "pass Hösl living in Frankfurt" in {
-    CommonSearcherFiltered.searchAsync(input = standardPerson.copy(lastName = "Hösl"), factory = PersonFactoryDE,
-      filterFn = filterFrankfurt).map(result => {
-      assert(result.length == 1)
-    })
-  }
+      override def createInstanceFromDocument(doc: Document): PkDataSet[Int] = PersonFactoryDE.createInstanceFromDocument(doc)
 
-  "Async search with async filter" should "exclude Mayer living not in Frankfurt" in {
-    import scala.concurrent.duration._
-    CommonSearcherFiltered.searchAsyncAsync(input = standardPerson.copy(lastName = "Mayer"), factory = PersonFactoryDE,
-      filterFn = filterFrankfurtAsync, filterTimeout = 10.seconds).map(result => {
-      assert(result.isEmpty)
-    })
-  }
+      override def setDataPool(data: Seq[Person]): Unit = ???
 
-  it should "pass Hösl living in Frankfurt" in {
-    CommonSearcherFiltered.searchAsyncAsync(input = standardPerson.copy(lastName = "Hösl"), factory = PersonFactoryDE,
-      filterFn = filterFrankfurtAsync).map(result => {
-      assert(result.length == 1)
+      override def populateDocument(document: Document, dataSet: Person): Unit = ???
+
+      override def createQuery(t: Person): Query = {
+        import de.crazything.search.CustomQuery.{data2Query, seq2Query}
+        Seq(
+          ("lastName", "Hösl").exact,
+          ("firstName", "Fr*").wildcard,
+          ("lastName", ".*").regex,
+          ("lastName", "Mayer").phonetic)
+      }
+
+      override def selectQueryCreator: (QueryCriteria, Person) => Query = ???
+    }
+
+//    def filterTrueOLD(result: SearchResult[Int, Person]): Future[Boolean] = Future {
+//      val timeout: Long = scala.util.Random.nextInt(500).toLong
+//      Thread.sleep(timeout)
+//      println(timeout)
+//      println(result)
+//      true
+//    }
+
+    def filterTrue(result: SearchResult[Int, Person]): Future[Boolean] = {
+      val promise: Promise[Boolean] = Promise[Boolean]
+
+      Future.apply({
+        val timeout: Long = scala.util.Random.nextInt(500).toLong
+        Thread.sleep(timeout)
+        println(timeout)
+        println(result)
+        promise.success(true)
+      })
+      println(s"Future method $result")
+      promise.future
+    }
+
+    def checkOrder(seq: Seq[SearchResult[Int, Person]]): Unit = {
+      def check(head: SearchResult[Int, Person], tail: Seq[SearchResult[Int, Person]]): Unit = {
+        val nextHead = tail.head
+        assert(head.score >= nextHead.score)
+        val nextTail = tail.tail
+        if (nextTail.nonEmpty) {
+          check(nextHead, nextTail)
+        }
+      }
+
+      check(seq.head, seq.tail)
+    }
+
+    CommonSearcherFiltered.searchAsyncAsync(input = standardPerson, factory = PersonFactoryAll,
+      filterFn = filterTrue).map(result => {
+      checkOrder(result)
+      assert(result.length == 6)
     })
   }
 

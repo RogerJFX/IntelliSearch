be6d85fe13d368d8ddb76737c4399de87e28665f
Roger <roger@crazything.de>
2018-09-27 20:06:22 +0200

    Not sure.

--- src/test/scala/de/crazything/app/test/CommonDeleteDataTest.scala
+++ src/test/scala/de/crazything/app/test/CommonDeleteDataTest.scala
@@ -3,15 +3,19 @@ package de.crazything.app.test
 import de.crazything.app.test.helpers.DataProvider
 import de.crazything.app.{GermanLanguage, Person, PersonFactoryDE}
 import de.crazything.search.{CommonIndexer, CommonSearcher, DirectoryContainer, QueryConfig}
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec, Matchers}
 
-class CommonDeleteDataTest extends FlatSpec with Matchers with QueryConfig with GermanLanguage with DirectoryContainer{
+class CommonDeleteDataTest extends FlatSpec with Matchers with BeforeAndAfterEach with QueryConfig with GermanLanguage with DirectoryContainer{
   
   val indexName = "DELETE_TEST_DATA"
 
   val standardPerson = Person(-1, "Herr", "firstName", "lastName", "street", "city")
 
-  CommonIndexer.index(DataProvider.readVerySimplePersons(), PersonFactoryDE, indexName)
+  override def beforeEach =
+    CommonIndexer.index(DataProvider.readVerySimplePersons(), PersonFactoryDE, indexName)
+
+
+
 
   "Persons" should "find Scharnofske" in {
     val searchResult = CommonSearcher.search(input = standardPerson.copy(lastName = "Scharnofske"), factory = PersonFactoryDE,
@@ -19,7 +23,7 @@ class CommonDeleteDataTest extends FlatSpec with Matchers with QueryConfig with 
     assert(searchResult.length == 1)
   }
 
-  it should "not find Scharnofske after deleting entry" in {
+  it should "not find Scharnofske after deleting entry with forceFlush = true" in {
     val personToDelete = standardPerson.copy(lastName = "Scharnofske")
     val searchResult = CommonSearcher.search(personToDelete, PersonFactoryDE, searcherOption = indexName)
     assert(searchResult.length == 1)
@@ -30,14 +34,14 @@ class CommonDeleteDataTest extends FlatSpec with Matchers with QueryConfig with 
     assert(afterResult.isEmpty)
   }
 
-//  it should "not find Hösl after deleting entry" in {
-//    val personToDelete = standardPerson.copy(lastName = "Hösl")
-//    val searchResult = CommonSearcher.search(personToDelete, PersonFactoryDE, searcherOption = indexName)
-//    assert(searchResult.length == 1)
-//    val foundPerson: Person = searchResult.head.found
-//    CommonIndexer.deleteData(Seq(foundPerson), PersonFactoryDE, indexName, true )
-//    val afterResult = CommonSearcher.search(personToDelete, PersonFactoryDE,
-//      searcherOption = indexName)
-//    assert(afterResult.isEmpty)
-//  }
+  it should "not find Scharnofske after deleting entry with forceFlush = false" in {
+    val personToDelete = standardPerson.copy(lastName = "Scharnofske")
+    val searchResult = CommonSearcher.search(personToDelete, PersonFactoryDE, searcherOption = indexName)
+    assert(searchResult.length == 1)
+    val foundPerson: Person = searchResult.head.found
+    CommonIndexer.deleteData(Seq(foundPerson), PersonFactoryDE, indexName, false )
+    val afterResult = CommonSearcher.search(personToDelete, PersonFactoryDE,
+      searcherOption = indexName)
+    assert(afterResult.isEmpty)
+  }
 }

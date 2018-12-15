d093130c97ec47608c23919a2318cbf67a500a58
Roger <roger@crazything.de>
2018-12-15 13:31:30 +0100

    Just optimized imports

--- src/main/scala/de/crazything/search/CommonSearcher.scala
+++ src/main/scala/de/crazything/search/CommonSearcher.scala
@@ -1,16 +1,14 @@
 package de.crazything.search
 
 import de.crazything.search.entity.{PkDataSet, QueryCriteria, SearchResult, SearchResultCollection}
-import de.crazything.search.ext.MappingSearcher.MAGIC_ONE_DAY
 import de.crazything.search.utils.FutureUtil
 import de.crazything.service.RestClient
 import org.apache.lucene.search.{IndexSearcher, Query, ScoreDoc}
-import play.api.libs.json.{Format, OFormat}
+import play.api.libs.json.OFormat
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.util.{Failure, Success}
+import scala.concurrent.{ExecutionContext, Future}
 
 object CommonSearcher extends MagicSettings{
 
@@ -62,14 +60,6 @@ object CommonSearcher extends MagicSettings{
                                         (implicit fmt: OFormat[T],
                                          rmt: OFormat[SearchResultCollection[I, T]],
                                          ec: ExecutionContext): Future[Seq[SearchResult[I, T]]] = {
-//    val promise = Promise[Seq[SearchResult[I, T]]]()
-//    val postFuture: Future[SearchResultCollection[I, T]] = RestClient.post[T, SearchResultCollection[I, T]](url, input)
-//    val timingOutFuture = FutureUtil.futureWithTimeout(postFuture, timeout)
-//    timingOutFuture.onComplete {
-//      case Success(r: SearchResultCollection[I, T]) => promise.success(r.entries)
-//      case Failure(t) => promise.failure(t)
-//    }
-//    promise.future
     val postFuture: Future[SearchResultCollection[I, T]] = RestClient.post[T, SearchResultCollection[I, T]](url, input)
     val timingOutFuture = FutureUtil.futureWithTimeout(postFuture, timeout)
     timingOutFuture.map(r => r.entries)
